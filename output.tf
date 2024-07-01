f4f6ddbbc6629fe883b4df7eff95406cbf6fbf8a
Tyler <tyleboyd8@gmail.com>
2024-07-01 19:45:45 -0400

    terraform update

--- output.tf
+++ output.tf
@@ -2,5 +2,5 @@ output "public-ip" {
   value = aws_instance.server1.public_ip
 }
 output "ssh-command" {
-  value = "ssh -i ${aws_instance.server1.key_name}.pem ubuntu@${aws_instance.server1.public_dns}"
+  value = "ssh -i ${aws_instance.server1.key_name}.pem ec2-user@${aws_instance.server1.public_dns}"
 }
